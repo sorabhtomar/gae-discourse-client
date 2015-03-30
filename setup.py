13ed2bf0491f38b1533a625d5d730e08df25db36
Thomas Davids <thomas@udacity.com>
2015-03-30 11:12:28 -0700

    fix(setup): fix setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     license="MIT",
     keywords="google appengine discourse api client",
     url="https://github.com/udacity/gae-discourse-client",
-    packages=['src', 'tests'],
-    long_description=read('README'),
+    packages=['gae_discourse_client', 'tests'],
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
